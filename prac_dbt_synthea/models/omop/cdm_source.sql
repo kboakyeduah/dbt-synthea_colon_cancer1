b871dab49bd6a4c627e510a154df1d9c6750e1aa
kboakyeduah <kluivertboakyejnr@gmail.com>
2025-08-17 12:03:18 +0100

    Casting variable data types in the cdm_source table

--- prac_dbt_synthea/models/omop/cdm_source.sql
+++ prac_dbt_synthea/models/omop/cdm_source.sql
@@ -1,14 +1,14 @@
 SELECT
-    'dbt-synthea' AS cdm_source_name
-    , 'dbt-synthea' AS cdm_source_abbreviation
-    , 'OHDSI' AS cdm_holder
-    , 'An OMOP CDM derived from a Synthea dataset using dbt.' AS source_description
+    'dbt-synthea'::varchar(255) AS cdm_source_name
+    , 'dbt-synthea'::varchar(25) AS cdm_source_abbreviation
+    , 'OHDSI'::varchar(255) AS cdm_holder
+    , 'An OMOP CDM derived from a Synthea dataset using dbt.'::varchar(255) AS source_description
     , 'https://synthetichealth.github.io/synthea/' AS source_documentation_reference
     , 'https://github.com/OHDSI/dbt-synthea' AS cdm_etl_reference
-    , current_date AS source_release_date
-    , current_date AS cdm_release_date
-    , '5.4' AS cdm_version
-    , vocabulary_version
-    , 798878 AS cdm_version_concept_id
+    , current_date::date AS source_release_date
+    , current_date::date AS cdm_release_date
+    , '5.4'::varchar(10) AS cdm_version
+    , vocabulary_version::varchar(20)
+    , 798878::integer AS cdm_version_concept_id
 FROM {{ ref('stg_vocabulary__vocabulary') }}
 WHERE vocabulary_id = 'None'
